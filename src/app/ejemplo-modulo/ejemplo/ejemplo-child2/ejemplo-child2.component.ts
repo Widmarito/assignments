971dffa7d9cce9573412904f65045e647b6e8b16
Jhoel Soncco <jhoel.soncco@provistacorp.com>
2023-04-18 08:51:43 -0500

    forms arrays inicio

--- src/app/ejemplo-modulo/ejemplo/ejemplo-child2/ejemplo-child2.component.ts
+++ src/app/ejemplo-modulo/ejemplo/ejemplo-child2/ejemplo-child2.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { FormArray, FormGroup } from '@angular/forms';
 import { Move } from '../model';
 
 @Component({
@@ -8,6 +9,9 @@ import { Move } from '../model';
 })
 export class EjemploChild2Component implements OnInit, OnDestroy {
   @Input() moves: Move[] = [];
+  formGroupMoves: FormGroup = new FormGroup({
+    arrayMoves: new FormArray([]),
+  });
   constructor() {}
   ngOnDestroy(): void {
     console.log('ejemplochild2 se ha destruido');
